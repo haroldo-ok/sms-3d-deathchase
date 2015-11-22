732eb4c69f373201fd83531dde438dffc033783f
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-11-22 19:56:03 -0200

    Created depth for X projection table

--- tools/gen_tables.py
+++ tools/gen_tables.py
@@ -5,16 +5,26 @@ PF_HEIGHT = 24
 
 MAX_DEPTH = 63
 TRUNK_WIDTH = 80
-MIN_TRUNK_WIDTH = 2
+MIN_TRUNK_WIDTH = 8
+
+MAX_X = PF_WIDTH
 
 trunk_table = []
+x_table = []
 max_divider = TRUNK_WIDTH / MIN_TRUNK_WIDTH
 for depth in range(0, MAX_DEPTH + 1):
     # TRUNK_WIDTH * n / 64 = 2 => n = 2 * 64 / TRUNK_WIDTH
     scale = 1.0 / (depth * max_divider / MAX_DEPTH + 1)
+
     trunk_table.append({
         'width': int(round(TRUNK_WIDTH * scale))
     })
+
+    x_for_depth = []
+    x_table.append(x_for_depth);
+    for x in range(0, MAX_X):
+        screen_x = scale * x
+        x_for_depth.append(int(screen_x))
 
 def to_record_array_const(records, keys):
     internal = ',\n\t'.join([to_record_const(r, keys) for r in records])
@@ -33,8 +43,10 @@ with open(script_dir + '/../three_d_tables.h', 'w') as f:
     f.write('#define PF_WIDTH %d\n' % PF_WIDTH)
     f.write('#define PF_HEIGHT %d\n' % PF_HEIGHT)
 
-    f.write('\n')
-    f.write('const trunk_data trunk_table[] = %s\n' %
+    f.write('\nconst trunk_data trunk_table[] = %s\n' %
             to_record_array_const(trunk_table, ['width']))
+
+    x_depth_string = ',\n\t'.join(['{%s}' % ','.join(map(str, l)) for l in x_table])
+    f.write('\nconst UBYTE trunk_depth_x[][%d] = {\n\t%s\n};\n' % (MAX_X, x_depth_string))
 
     f.write('\n#endif /* _THREE_D_TABLES_H */\n')
